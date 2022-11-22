5dc46b5e150a7e787ae8af297540f2d47d11a5b0
ytkachov <yuri.tkachov@gmail.com>
2022-11-22 22:32:21 +0700

    достал старые файлы из старой ветки

---
+++ PictureSlideshowScreensaver/Models/LeapMotionListener.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+using Microsoft.Win32;
+
+using Leap;
+
+namespace LeapMotion
+{
+
+  public class LeapMotionListener : Listener
+  {
+    private Controller _controller = new Controller();
+    private bool _connected;
+    private bool _present;
+
+    public bool connected
+    {
+      get
+      {
+        return _connected;
+      }
+    }
+
+    public bool present
+    {
+      get
+      {
+        return _present;
+      }
+    }
+
+
+    public LeapMotionListener()
+    {
+      _controller.AddListener(this);
+    }
+
+    public override void OnInit(Controller controller)
+    {
+      _present = true;
+    }
+
+    public override void OnConnect(Controller controller)
+    {
+      _connected = true;
+
+      controller.SetPolicy(Controller.PolicyFlag.POLICY_DEFAULT);
+      controller.EnableGesture(Gesture.GestureType.TYPE_CIRCLE);
+      controller.EnableGesture(Gesture.GestureType.TYPE_KEY_TAP);
+      controller.EnableGesture(Gesture.GestureType.TYPE_SCREEN_TAP);
+
+      controller.EnableGesture(Gesture.GestureType.TYPE_SWIPE);
+      controller.Config.SetFloat("Gesture.Swipe.MinLength", 100.0f);
+    }
+
+    public override void OnDisconnect(Controller controller)
+    {
+      _connected = true;
+    }
+
+    public override void OnExit(Controller controller)
+    {
+      _present = false;
+    }
+
+    public void Close()
+    {
+      _controller.RemoveListener(this);
+      _controller.Dispose();
+    }
+  }
+}
---
+++ PictureSlideshowScreensaver/ViewModels/LeapMotionViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Threading;
+
+using LeapMotion;
+
+namespace informers
+{
+  public enum Fingers
+  {
+    None,
+    RightFive,
+    RightFiveSpread
+  }
+
+  public class LeapMotionInformer : INotifyPropertyChanged
+  {
+    private DispatcherTimer _lm_Tick = new DispatcherTimer();
+    private Fingers _hand_config;
+
+    public Fingers HandConfig
+    {
+      get { return _hand_config; }
+      set { _hand_config = value; RaisePropertyChanged(); }
+    }
+
+    public LeapMotionInformer()
+    {
+      _lm_Tick.Tick += new EventHandler(controller_Tick);
+      _lm_Tick.Interval = TimeSpan.FromMilliseconds(50.0);
+      _lm_Tick.Start();
+    }
+
+    void controller_Tick(object sender, EventArgs e)
+    {
+    }
+
+    public void Close()
+    {
+    }
+
+    // INotifyPropertyChanged
+    public event PropertyChangedEventHandler PropertyChanged;
+    private void RaisePropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string caller_name = null)
+    {
+      if (PropertyChanged != null)
+        PropertyChanged(this, new PropertyChangedEventArgs(caller_name));
+    }
+  }
+}
